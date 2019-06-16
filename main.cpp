5cf3caa5adb0f70a4204b5e7131ac637c1807b4a
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-06-16 15:13:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <string>
 #include <list>
 #include "Archiver.h"
 
 using namespace std;
 
-int main()
+int main(/*int argc, char** argv*/)
 {
-	coder("text.txt", "out.txt");
-	decoder("out.txt", "text_out.txt");
+	setlocale(LC_ALL, "Russian");
+
+	/*if (argc < 3)
+	{
+		cout << "Пример вызова программы: nameprogram.exe inFileName outFileName" << endl;
+	}*/
+
+	cout << "Welcome!" << endl;
+
+	const char* inFileName = "out.txt"; // argv[1];
+	const char* outFileName = "text_out.txt"; //argv[2];
+
+	cout << "Если хотите архивировать введите coder, разархивировать - decoder: ";
+	string action;
+	getline(cin, action);
+
+	if (action == "coder")
+	{
+		coder(inFileName, outFileName);
+		cout << "Архивирование завершено" << endl;
+	}
+	else if (action == "decoder")
+	{
+		decoder(inFileName, outFileName);
+		cout << "Разархивирование завершено" << endl;
+	}
+	else
+		cout << "Вы неправильно ввели команду" << endl;
 
 	system("pause");
 	return 0;
